e01c157235da20191a3a2f650d8218e50acdca49
HamzaRadaideh <hjradaideh10@gmail.com>
2024-07-17 11:51:19 +0300

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebDinark1.Models;
+
+namespace WebDinark1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<WebDinark1.Models.Joke> Joke { get; set; } = default!;
+    }
+}
